48b2559af2b04cdb6f1f09271fd2e7ab48bac7d3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-29 11:47:29 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Export Data'] = 'Flytja út gögn';
+$lang['Export albums'] = 'Flytja út albúm';
+$lang['Export comments'] = 'Flytja út umsagnir';
+$lang['Export photos'] = 'Flytja út myndir';
