0399824115c1fd651d34176f3b9591da7e9cc572
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-08 17:32:55 +0200

    Add zh_TW, thanks to closer

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Export Data'] = '匯出資料';
+$lang['Export albums'] = '匯出相簿';
+$lang['Export comments'] = '匯出評論';
+$lang['Export photos'] = '匯出照片';
