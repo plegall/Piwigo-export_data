2f8f71ab3b6b2e1ac1481b9562a7e28891b2c6c7
plegall <plg@piwigo.org>
2023-10-09 10:48:54 +0200

    add missing translations

--- language/en_UK/plugin.lang.php
+++ language/en_UK/plugin.lang.php
@@ -3,4 +3,6 @@ $lang['Export Data'] = 'Export Data';
 $lang['Export albums'] = 'Export albums';
 $lang['Export photos'] = 'Export photos';
 $lang['Export comments'] = 'Export comments';
+$lang['Export downloads'] = 'Export downloads';
+$lang['Export users'] = 'Export users';
 ?>
--- language/fr_FR/plugin.lang.php
+++ language/fr_FR/plugin.lang.php
@@ -3,4 +3,6 @@ $lang['Export Data'] = 'Extraction de données';
 $lang['Export albums'] = 'Extraire les albums';
 $lang['Export photos'] = 'Extraire les photos';
 $lang['Export comments'] = 'Extraire les commentaires';
+$lang['Export downloads'] = 'Extraire les téléchargement';
+$lang['Export users'] = 'Extraire les utilisateurs';
 ?>
