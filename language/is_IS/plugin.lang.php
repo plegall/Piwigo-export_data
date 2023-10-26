924901504f016cadf11318ec20eb66fd361382c5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-10-26 15:41:57 +0200

    Update is_IS, thanks to gudjonsi

--- language/is_IS/plugin.lang.php
+++ language/is_IS/plugin.lang.php
@@ -9,3 +9,5 @@ $lang['Export Data'] = 'Flytja út gögn';
 $lang['Export albums'] = 'Flytja út albúm';
 $lang['Export comments'] = 'Flytja út umsagnir';
 $lang['Export photos'] = 'Flytja út myndir';
+$lang['Export downloads'] = 'Flytja út niðurhal';
+$lang['Export users'] = 'Flytja út notendur';
