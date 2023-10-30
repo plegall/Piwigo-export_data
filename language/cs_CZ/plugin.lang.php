e52464bdd6dbc5a622e9db04e0fe45a02071ceb1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-10-30 10:18:35 +0100

    Update cs_CZ, thanks to MiPi

--- language/cs_CZ/plugin.lang.php
+++ language/cs_CZ/plugin.lang.php
@@ -3,3 +3,5 @@ $lang['Export Data'] = 'Exportovat data';
 $lang['Export albums'] = 'Exportovat alba';
 $lang['Export photos'] = 'Exportovat fotografie';
 $lang['Export comments'] = 'Exportovat komentáře';
+$lang['Export downloads'] = 'Exportovat stažené soubory';
+$lang['Export users'] = 'Exportovat uživatele';
