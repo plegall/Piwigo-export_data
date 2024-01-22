844fa93485c33186c7d0df6703451e721f5f0ea8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-01-22 05:22:03 +0100

    Update zh_TW, thanks to closer

--- language/zh_TW/plugin.lang.php
+++ language/zh_TW/plugin.lang.php
@@ -9,3 +9,5 @@ $lang['Export Data'] = '匯出資料';
 $lang['Export albums'] = '匯出相簿';
 $lang['Export comments'] = '匯出評論';
 $lang['Export photos'] = '匯出照片';
+$lang['Export downloads'] = '匯出下載資料';
+$lang['Export users'] = '匯出使用者資訊';
