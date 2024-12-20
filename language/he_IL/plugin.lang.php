4a820392f7b1f51e7102ab5a6930a6bcfce3b934
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 08:55:34 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Export Data'] = 'ייצוא נתונים';
+$lang['Export albums'] = 'ייצוא אלבומים';
+$lang['Export comments'] = 'ייצוא תגובות';
+$lang['Export downloads'] = 'ייצוא הורדות';
+$lang['Export photos'] = 'ייצוא תמונות';
+$lang['Export users'] = 'ייצוא משתמשים';
