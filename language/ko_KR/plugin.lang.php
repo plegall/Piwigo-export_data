cde1c8d1ee3a7226499cac1312ded9837360d69b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-08 08:14:05 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Export Data'] = '데이터 내보내기';
+$lang['Export albums'] = '앨범 내보내기';
+$lang['Export comments'] = '댓글 내보내기';
+$lang['Export downloads'] = '다운로드 내보내기';
+$lang['Export photos'] = '사진 내보내기';
+$lang['Export users'] = '사용자 내보내기';
